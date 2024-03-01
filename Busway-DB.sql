413ccaa3f667e454ee92c890b5e08411df0c3317
AdZrk <64109928+AdZrk@users.noreply.github.com>
2024-02-29 18:46:37 -0800

    Update Busway-DB.sql

--- Busway-DB.sql
+++ Busway-DB.sql
@@ -9,3 +9,11 @@ CREATE TABLE Line
 
 INSERT INTO Line VALUES (COLOR,LETTER,MI_BUS,NUM_STOPS_STAT) VALUES ('Orange','G',17.7,17)
 INSERT INTO Line VALUES ('Silver','J',38,12,31)
+
+CREATE TABLE Stop
+(
+  NAME         VARCHAR2(30),
+  LINE         VARCHAR2(1),
+  TYPE         ENUM('Station','Street')
+  NUM_IN_LINE  INT
+);
