6e20c4d0bedcaf83612211785b5c4909ffe30e9d
AdZrk <64109928+AdZrk@users.noreply.github.com>
2024-03-05 22:29:17 -0800

    Update Bike-DB.sql

--- Bike-DB.sql
+++ Bike-DB.sql
@@ -7,4 +7,8 @@ CREATE TABLE Station
 );
 
 INSERT INTO Station (ST1,ST2,DOCKS) VALUES 
-    ('Lankershim','Califa',3),('Chandler','Laurel Canyon',8),('Chandler','Colfax',12),
+    ('Lankershim','Califa',3),('Chandler','Laurel Canyon',8),('Chandler','Colfax',12),('Chandler','Cahuenga',8),('Magnolia','Cartwright',6),
+    ('Magnolia','Clybourn',4),('Lankershim','Hesby',6),('Lankershim','Vineland',6),('Riverside','Cahuenga',9),('Mariota','Riverside',8),
+    (
+INSERT INTO Station VALUES 
+    ('Lankershim','Chandler','North Hollywood Station',14),('Lankershim','McCormick','North Hollywood Plaza',2),
