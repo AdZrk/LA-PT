ce06446e17e52a31de1058c9c874da4282737a6e
AdZrk <64109928+AdZrk@users.noreply.github.com>
2024-03-05 16:50:33 -0800

    Create Bike-DB.sql

---
+++ Bike-DB.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Station
+(
+    ST1      VARCHAR2(20),
+    ST2      VARCHAR2(20),
+    ALTNAME  VARCHAR2(30),
+    DOCKS    INT
+);
+
+INSERT INTO Station (ST1,ST2,DOCKS) VALUES 
+    ('Lankershim','Califa',3),('Chandler','Laurel Canyon',8),('Chandler','Colfax',12),
