0b9e95130f99c98146bbc0f3cf054327336e79e3
AdZrk <64109928+AdZrk@users.noreply.github.com>
2024-03-06 16:19:16 -0800

    Update Bike-DB.sql

--- Bike-DB.sql
+++ Bike-DB.sql
@@ -7,8 +7,27 @@ CREATE TABLE Station
 );
 
 INSERT INTO Station (ST1,ST2,DOCKS) VALUES 
-    ('Lankershim','Califa',3),('Chandler','Laurel Canyon',8),('Chandler','Colfax',12),('Chandler','Cahuenga',8),('Magnolia','Cartwright',6),
-    ('Magnolia','Clybourn',4),('Lankershim','Hesby',6),('Lankershim','Vineland',6),('Riverside','Cahuenga',9),('Mariota','Riverside',8),
-    (
+    ('Lankershim','Califa',10),('Chandler','Laurel Canyon',15),('Chandler','Colfax',18),('Chandler','Cahuenga',12),('Magnolia','Cartwright',12),
+    ('Magnolia','Clybourn',11),('Lankershim','Hesby',11),('Lankershim','Vineland',12),('Riverside','Cahuenga',18),('Mariota','Riverside',13),
+    ('Franklin','Fuller',22),('Yucca','Argyle',23),('Highland','Hollywood',11),('McCadden','Hollywood',14),('Whitley','Hollywood',13),
+    ('Ivar','Hollywood',15),('Argyle','Hollywood',17),('Hawthorn','Orange',11),('Highland','Sunset',23),('Wilcox','Fountain',15),
+    ('Fountain','Vine',14),('Western','Hollywood',10),('Normandie','Hollywood',14),('Normandie','Sunset',9),('Ambrose','Hillhurst',9),
+    ('Rowena','Hyperion',18),('Vermont','Franklin',9),('Franklin','Hillhurst',13),('Tracy','Hyperion',13),('Glendale','Silver Lake',12),
+    ('Melbourne','Vermont',10),('Hillhurst','Kingswell',10),('Hollywood','Rodney',8),('Maubert','Vermont',10),('Sunset','Virgil',10),
+    ('Hoover','Mountain',13),('Vermont Pl','Vermont',10),('Santa Monica','Sunset',17),('Santa Monica','Virgil',14),('Edgemont','Melrose',18),
+    ('Lucile','Hoover',6),('Sunset','Silver Lake',14),('Vermont','Beverly',11),('Vendome','Beverly',8),('Park','Echo Park',14),
+    ('Laveta Terrace','Sunset',9),('Sunset','Douglas',15),('Western','3rd',15),('Kenmore','3rd',12),('5th','Western',8),
+    ('4th','Vermont',15),('Wilshire','Hobart',9),('Wilshire','Normandie',12),('Wilshire','Alexandria',14),('Kenmore','6th',16),
+    ('Shatto Pl','Wilshire',9),('Virgil','6th',15),('Normandie','8th',4),('7th','Berendo',15),('8th','Vermont',15),
+    ('7th','Westmoreland',15),('Ardmore','Olympic',15),('Berendo','Olympic',18),('Rampart','Wilshire',15),('7th','Park View',15),
+    ('James M Wood','Alvarado',13),('Westlake','3rd',10),('Union','7th',12),('Wilshire','Witmer',8),('7th','Bixel',33),
+    ('Union','Olympic',14),('Albany','Olympic',15),('12th','Valencia',13),('Figueroa','Cesar E Chavez',15),('Hill','College',13),
+    ('Spring','College',15),('New High','Ord',15),('2nd','Figueroa',18),('Hope','1st',16),('Grand','Temple',14),
+    ('Figueroa','4th',15),('Grand','3rd',14),('2nd','Hill',17),('Main','1st',29),('Los Angeles','Temple',30),
+    ('Broadway','3rd',23),('Spring','3rd',14),('Main','Winston',12),('3rd','San Pedro',15),('San Pedro','Winston',11),
+    ('1st','Central',28),('Traction','Rose',24),(
 INSERT INTO Station VALUES 
-    ('Lankershim','Chandler','North Hollywood Station',14),('Lankershim','McCormick','North Hollywood Plaza',2),
+    ('Lankershim','Chandler','North Hollywood Station',20),('Lankershim','McCormick','North Hollywood Plaza',9),('Sunset','Maltman','Sunset Triangle Plaza',15),
+    ('Sunset','Mohawk','Mohawk Bend',10),('Wilshire','Western','Wiltern',11),('James M Wood','Western','Koreatown Plaza',24),
+    ('Westlake','Wilshire','Westlake/MacArthur Park Station',15),('1st','2nd','Vista Hermosa Park',16),('Figueroa','Diamond','Figueroa Plaza',14),
+    ('Alameda','Los Angeles','Union Station West Portal',30),('Vignes','Cesar E Chavez','Union Station East Portal',17),(
