496f9d426209500a8cf48941b8cf9c3f962d8e83
AdZrk <64109928+AdZrk@users.noreply.github.com>
2024-03-10 20:31:33 -0700

    Update Metrolink-DB.sql

--- Metrolink-DB.sql
+++ Metrolink-DB.sql
@@ -1,6 +1,17 @@
 CREATE TABLE Line
 (
-  NAME
-  MI            
+  NAME          VARCHAR2(30),
+  MI            DOUBLE PRECISION,
   NUM_STATIONS  INT
+);
+
+INSERT INTO Line VALUES ('Antelope Valley',76.6,13),('Inland Empire-Orange County',100.1,16),('Orange County',87.2,15),
+                        ('Riverside',59.1,7),('San Bernardino',57.6,17),('Ventura County',70.9,12),
+                        ('91/Perris Valley',83.8,13),('Arrow',9,5)
+
+CREATE TABLE Station
+(
+    NAME         VARCHAR2(30),
+    LINE_ABBR    VARCHAR2(5),
+    NUM_IN_LINE  INT
 );
