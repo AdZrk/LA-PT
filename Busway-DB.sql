1f1c7476dc0a2530093cd005daff4d1fdbd1652a
AdZrk <64109928+AdZrk@users.noreply.github.com>
2024-03-12 22:54:17 -0700

    Update Busway-DB.sql

--- Busway-DB.sql
+++ Busway-DB.sql
@@ -20,10 +20,18 @@ CREATE TABLE Stop
 );
 
 INSERT INTO Stop (NAME,TYPE,SERV,DIR) VALUES 
-    ('El Monte Station','Sta',910,'NB'),('Cal State LA','Sta',910,'NB'),('Los Angeles General Medical Center','Sta',910,'NB'),('Union Station','Sta',910,'NB'),
-    ('Aliso/Los Angeles','Str',910,'NB'),('Spring/1st','Str',910,'NB'),('1st/Hill','Str',910,'NB'),('Olive/Kosciuszko','Str',910,'NB'),
-    ('Olive/5th','Str',910,'NB'),('6th/Flower','Str',910,'NB'),('Figueroa/7th','Str',910,'NB'),('Figueroa/Olympic','Str',910,'NB'),
-    ('Figueroa/Pico','Str',910,'NB'),('Figueroa/Washington','Str',910,'NB'),('Figueroa/23rd','Str',910,'NB'),('Figueroa Way/Adams','Str',910,'NB'),
-    ('37th/USC','Sta',910,'NB'),('Slauson','Sta',910,'NB'),('Manchester','Sta',910,'NB'),('Harbor Freeway','Sta',910,'NB'),
-    ('Rosecrans','Sta',910,'NB'),('Harbor Gateway Transit Center','Sta',910,'NB');
+    ('El Monte Station','Sta',910,'SB'),('Cal State LA','Sta',910,'SB'),('Los Angeles General Medical Center','Sta',910,'SB'),('Union Station','Sta',910,'SB'),
+    ('Arcadia/Los Angeles','Str',910,'SB'),('Spring/1st','Str',910,'SB'),('1st/Hill','Str',910,'SB'),('Grand/3rd','Str',910,'SB'),
+    ('Grand/5th','Str',910,'SB'),('Flower/7th','Str',910,'SB'),('Flower/Olympic','Str',910,'SB'),
+    ('Flower/Pico','Str',910,'SB'),('Flower/Washington','Str',910,'SB'),('Flower/23rd','Str',910,'SB'),('Flower/Adams','Str',910,'SB'),
+    ('37th/USC','Sta',910,'SB'),('Slauson','Sta',910,'SB'),('Manchester','Sta',910,'SB'),('Harbor Freeway','Sta',910,'SB'),
+    ('Rosecrans','Sta',910,'SB'),('Harbor Gateway Transit Center','Sta',910,'SB');
+ALTER TABLE Stop AUTO_INCREMENT = 1;
+INSERT INTO Stop (NAME,TYPE,SERV,DIR) VALUES 
+    ('Harbor Gateway Transit Center','Sta',910,'NB'),('Rosecrans','Sta',910,'NB'),('Harbor Freeway','Sta',910,'NB'),('Manchester','Sta',910,'NB'),
+    ('Slauson','Sta',910,'NB'),('37th/USC','Sta',910,'NB'),('Figueroa Way/Adams','Str',910,'NB'),('Figueroa/23rd','Str',910,'NB'),
+    ('Figueroa/Washington','Str',910,'NB'),('Figueroa/Pico','Str',910,'NB'),('Figueroa/Olympic','Str',910,'NB'),('Figueroa/7th','Str',910,'NB'),
+    ('6th/Flower','Str',910,'NB'),('Olive/5th','Str',910,'NB'),('Olive/Kosciuszko','Str',910,'NB'),('1st/Hill','Str',910,'NB'),
+    ('Spring/1st','Str',910,'NB'),('Aliso/Los Angeles','Str',910,'NB'),('Union Station','Sta',910,'NB'),('Los Angeles General Medical Center','Sta',910,'NB'),
+    ('Cal State LA','Sta',910,'NB'),('El Monte Station','Sta',910,'NB');
 ALTER TABLE Stop AUTO_INCREMENT = 1;
